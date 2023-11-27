5debd8a8f23debf3e7bbd64d86d8329c254e0324
AMSusz <casimir3thegreat@gmail.com>
2023-11-27 01:15:20 +0100

    update 27.11.2023

--- l9/l9cw1.php
+++ l9/l9cw1.php
@@ -7,26 +7,27 @@
     <style>
     <?php
     function select_if_selected($method = "get", $select_name, $option_name){
-        echo $method;
-        if (strtolower($method) == 'get') if(isset($_GET[$select_name]) && $_GET[$select_name] == $option_name) echo "selected";
-        else if (strtolower($method) == 'post') if(isset($_POST[$select_name]) && $_POST[$select_name] == $option_name) echo "selected";
+        if (strtolower($method) == "get") { if(isset($_GET[$select_name]) && $_GET[$select_name] == $option_name) echo "selected"; }
+        else if (strtolower($method) == "post") { if(isset($_POST[$select_name]) && $_POST[$select_name] == $option_name) echo "selected"; }
     }
 
-    $color = $_POST['color']?? "red";
-    echo "body{background-color: $color}";
-
     $METHOD = "POST";
+
+    if ($METHOD == "POST") $color = $_POST["colors"] ?? "red";
+    else if ($METHOD == "GET") $color = $_GET["colors"] ?? "red";
+
+    echo "body{background-color: $color;}";
     ?>
     </style>
 </head>
 <body>
     <h3>Wybór koloru tła strony</h3>
-    <form method="{{$METHOD}}">
+    <form method=<?php echo $METHOD; ?>>
         <label for="colors">Wybierz kolor tła:</label><br><br>
-        <select name="color" id="colors">
-            <option value="red" <?php select_if_selected("post", "colors", "red") ?>>czerwony</option>
-            <option value="green" <?php select_if_selected("post", "colors", "green") ?>>zielony</option>
-            <option value="blue" <?php select_if_selected("post", "colors", "blue") ?>>niebieski</option>
+        <select name="colors" id="colors">
+            <option value="red" <?php select_if_selected("$METHOD", "colors", "red") ?>>czerwony</option>
+            <option value="green" <?php select_if_selected("$METHOD", "colors", "green") ?>>zielony</option>
+            <option value="blue" <?php select_if_selected("$METHOD", "colors", "blue")  ?>>niebieski</option>
         </select>
         <br><br>
         <input type="submit" value="Zmień kolor strony">
--- l9/l9cw2.php
+++ l9/l9cw2.php
@@ -4,38 +4,84 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cw2</title>
+    <style>
+        .error-box{
+            color: red;
+            height: 30px;
+            margin-top: 10px;
+        }
+    </style>
 </head>
 <body>
     <?php
+    function select_if_selected($my_name, $goal){
+        if ($my_name == $goal) echo "checked";
+    }
+
+    function validate_number_field($value){
+        if (strlen($value)==0) return "Pole nie może być puste";
+        else if (!is_numeric($value)) return "Musi być wartość liczbowa";
+    }
+
     // variable declaration
-    
+    $a = trim($_GET["a"])?? null;
+    $b = trim($_GET["b"])?? null;
+    $o = $_GET["operation"]?? null;
+
+    $r = null;
+    if ($a !== null && $b !== null){
+        if ($o == "+") $r = $a+$b;
+        else if ($o == "-") $r = $a-$b;
+        else if ($o == "*") $r = $a*$b;
+        else if ($o == "/" && $b!=0) $r = $a/$b;
+    }
+
     ?>
     <form>
         <label for="a">Wpisz 1. liczbę: </label><br><br>
-        <input type="number" id="a" name="a" min="-100" max="100">
+        <input type="number" id="a" name="a" min="-100" max="100" value="<?php echo $a ?>">
         <br>
         <div class="error-box">
-
+        <?php 
+        echo validate_number_field($a);
+        ?>
         </div>
-        <br>
         <label for="b">Wpisz 2. liczbę: </label>
         <br><br>
-        <input type="number" id="b" name="b" min="-100" max="100">
+        <input type="number" id="b" name="b" min="-100" max="100" value="<?php echo $b ?>">
         <br>
-        <div class="error-box"></div>
-        <br>
+        <div class="error-box">
+        <?php 
+        echo validate_number_field($b);
+        ?>
+        </div>
         <label>Wybierz dzialanie:</label>
         <br><br>        
-        <input type="radio" id="sum" name="sum">
+        <input type="radio" id="sum" name="operation" value="+" <?php select_if_selected("+", $o) ?>>
         <label for="sum">+</label><br>
-        <input type="radio" id="diff" name="difference">
+        <input type="radio" id="diff" name="operation" value="-" <?php select_if_selected("-", $o) ?>>
         <label for="diff">-</label><br>
-        <input type="radio" id="product" name="product">
+        <input type="radio" id="product" name="operation" value="*" <?php select_if_selected("*", $o) ?>>
         <label for="product">*</label><br>
-        <input type="radio" id="quotient" name="quotient">
+        <input type="radio" id="quotient" name="operation" value="/" <?php select_if_selected("/", $o) ?>>
         <label for="quotient">/</label>
         <br><br>
         <input type="submit" value="Wykonaj" name="post">
 </form>
+    <p class="chosen-numbers">
+        <?php
+        echo "Wybrane liczby: $a i $b";
+        ?>
+    </p>
+    <h3>
+        <?php
+        if ($r!==null) echo "$a $o $b = $r";
+        ?>
+    </h3>
+    <h3 style="color: red;">
+        <?php
+        if ($o == "/" && $b == 0) echo "Nie dziel przez 0!!!";
+        ?>
+    </h3>
 </body>
 </html>
